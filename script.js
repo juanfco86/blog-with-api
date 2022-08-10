3a945ff0730223486f2d663d2f32cdfc48b7f10c
Bernat Pineda <bernatpinedatorrens@gmail.com>
2022-08-10 14:24:33 +0200

    let-for

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
     .then((response) => response.json())
     .then((json) => console.log(json));
 
+for (let i=0; i<5; i++){
+  
+}
 //  fetch("http://localhost:3000/users")
 //    .then((response) => response.json())
 //    .then((json) => console.log(json));
