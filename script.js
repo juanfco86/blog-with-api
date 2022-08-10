e17abf01ebb837e7939396f1ccfa658eb481e6db
juanfco86 <juanfco86@gmail.com>
2022-08-10 16:06:55 +0200

    5 posts finished

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
+//VARIABLES
 const containerTitles = document.querySelector(".container-title");
 const containerTitles2 = document.querySelector(".container-title2");
+const containerTitles3 = document.querySelector(".container-title3");
+const containerTitles4 = document.querySelector(".container-title4");
+const containerTitles5 = document.querySelector(".container-title5");
 
+//FUNCIONES
+
+
+// POST
 fetch("http://localhost:3000/posts/1", {
   method: "GET",
 })
   .then(function (response) {
     return response.json();
   })
-  .then(function (json) {
+  .then(function (data) {
     // containerCat.innerHTML = <img src="${datos.file}" />;
-    containerTitles.innerHTML = `<h5 class="mb-1 h5-titles">"${json.title}"</h5>`;
+    containerTitles.innerHTML = `<h5 class="mb-1 h5-titles">"${data.title}"</h5>`;
     console.log(containerTitles);
-    console.log(json);
+    console.log(data);
   });
 
 fetch("http://localhost:3000/posts/2", {
@@ -20,13 +28,50 @@ fetch("http://localhost:3000/posts/2", {
   .then(function (response) {
     return response.json();
   })
-  .then(function (json) {
+  .then(function (data) {
     // containerCat.innerHTML = <img src="${datos.file}" />;
-    containerTitles2.innerHTML = `<h5 class="mb-1 h5-titles">"${json.title}"</h5>`;
+    containerTitles2.innerHTML = `<h5 class="mb-1 h5-titles">"${data.title}"</h5>`;
     console.log(containerTitles);
-    console.log(json);
+    console.log(data);
   });
+  
+  fetch("http://localhost:3000/posts/3", {
+    method: "GET",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // containerCat.innerHTML = <img src="${datos.file}" />;
+      containerTitles3.innerHTML = `<h5 class="mb-1 h5-titles">"${data.title}"</h5>`;
+      console.log(containerTitles);
+      console.log(data);
+    });
+  
+    fetch("http://localhost:3000/posts/4", {
+      method: "GET",
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // containerCat.innerHTML = <img src="${datos.file}" />;
+        containerTitles4.innerHTML = `<h5 class="mb-1 h5-titles">"${data.title}"</h5>`;
+        console.log(containerTitles);
+        console.log(data);
+      });
+  
+fetch("http://localhost:3000/posts/5", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // containerCat.innerHTML = <img src="${datos.file}" />;
+    containerTitles5.innerHTML = `<h5 class="mb-1 h5-titles">"${data.title}"</h5>`;
+    console.log(containerTitles);
+    console.log(data);
+  });
+  
 
-//  fetch("http://localhost:3000/users")
-//    .then((response) => response.json())
-//    .then((json) => console.log(json));
