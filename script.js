9cf029bee4f5ea265308ef9ff09e31997dd1daa4
juanfco86 <juanfco86@gmail.com>
2022-08-10 13:34:27 +0200

    nav bar, container blog, blog items. HTML

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 
-  fetch("http://localhost:3000/posts")
+
+  fetch("http://localhost:3000/posts", {
+    method: "GET",
+  })
     .then((response) => response.json())
-    .then((json) =>console.log(json));
+    .then((json) => console.log(json));
 
 //  fetch("http://localhost:3000/users")
 //    .then((response) => response.json())
