5041f3aad9e0acceb05973d2837ba8885b0bec51
juanfco86 <juanfco86@gmail.com>
2022-08-10 14:16:38 +0200

    blog API conection 1.0

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
+const containerTitles = document.querySelector(".container-title");
+const containerTitles2 = document.querySelector(".container-title2");
 
-
-  fetch("http://localhost:3000/posts", {
-    method: "GET",
+fetch("http://localhost:3000/posts/1", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
   })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  .then(function (json) {
+    // containerCat.innerHTML = <img src="${datos.file}" />;
+    containerTitles.innerHTML = `<h5 class="mb-1 h5-titles">"${json.title}"</h5>`;
+    console.log(containerTitles);
+    console.log(json);
+  });
+
+fetch("http://localhost:3000/posts/2", {
+  method: "GET",
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    // containerCat.innerHTML = <img src="${datos.file}" />;
+    containerTitles2.innerHTML = `<h5 class="mb-1 h5-titles">"${json.title}"</h5>`;
+    console.log(containerTitles);
+    console.log(json);
+  });
 
 //  fetch("http://localhost:3000/users")
 //    .then((response) => response.json())
